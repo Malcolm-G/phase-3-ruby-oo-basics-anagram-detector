9bc525f0dbb8693d64593d50cb16dd9e44c1fc89
Malcolm-G <malcolm.githuka@gmail.com>
2023-02-08 14:40:22 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (list)
+        new_list = list.filter do |word|
+            @word.chars.sort == (word.chars).sort
+        end
+
+        return new_list
+    end
+
+end
